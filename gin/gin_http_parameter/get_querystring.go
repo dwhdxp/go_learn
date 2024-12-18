4326ce25c8837b30322f6136a67c62a2ce586c2f
dxp <dxp20001107@163.com>
2024-12-18 21:25:49 +0800

    gin get_param

---
+++ gin/gin_http_parameter/get_querystring.go
@@ -0,0 +1,31 @@
+/*
+通常GET请求时，会在URL中使用querystring来传输参数，这些参数以键值对的形式附加在URL的末尾，通常由?开始，用&分隔
+gin提供Query()、DefaultQuery()、GetQuery()等来获取querystring
+*/
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		// 1.name := c.Query("query") // 获取指定key的value
+		// 2.name := c.DefaultQuery("query", "lxz") // 获取指定value，若没有返回设置的默认值
+		name, ok := c.GetQuery("query")
+		if !ok {
+			name = "nobody"
+		}
+		age := c.Query("age")
+		msg := c.DefaultQuery("msg", "hello lxz")
+		c.JSON(http.StatusOK, gin.H{
+			"name": name,
+			"age":  age,
+			"msg":  msg,
+		})
+	})
+
+	router.Run(":9091")
+}
